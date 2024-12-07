5ed352a068186af4aebeca4caf5ad7794080999e
Jordan Trana [Jono Tho'ra] <jordan.trana@gmail.com>
2024-12-06 20:51:43 -0800

    adding look ahead buttons

--- CosmicCypherApp/CosmicCypher/v4 Screens/ViewControllers/StarChartSelectViewController.swift
+++ CosmicCypherApp/CosmicCypher/v4 Screens/ViewControllers/StarChartSelectViewController.swift
@@ -26,6 +26,64 @@ class StarChartSelectViewController: UIViewController {
         ResonanceReportViewController.current?.update()
         ResonanceReportViewController.current?.renderStarChart()
         
-        self.dismiss(animated: true, completion: nil)
+        if let p = self.parent {
+            p.dismiss(animated: true)
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
     }
+    
+    @IBAction func tomorrowButtonTouch(_ sender: UIButton) {
+        guard let date = Date.beginningOf(.tomorrow) else {
+            print("FAILED: tomorrowButtonTouch")
+            return
+        }
+        
+        StarChart.Core.current = StarChart(date: date, coordinates: StarChart.Core.current.coordinates)
+        ResonanceReportViewController.current?.update()
+        ResonanceReportViewController.current?.renderStarChart()
+        
+        if let p = self.parent {
+            p.dismiss(animated: true)
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func twoDaysAheadButtonTouch(_ sender: UIButton) {
+        guard let date = Date.beginningOf(.dayAfterToday(2)) else {
+            print("FAILED: twoDaysAheadButtonTouch")
+            return
+        }
+        
+        StarChart.Core.current = StarChart(date: date, coordinates: StarChart.Core.current.coordinates)
+        ResonanceReportViewController.current?.update()
+        ResonanceReportViewController.current?.renderStarChart()
+        
+        if let p = self.parent {
+            p.dismiss(animated: true)
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func threeDaysAheadButtonTouch(_ sender: UIButton) {
+        guard let date = Date.beginningOf(.dayAfterToday(3)) else {
+            print("FAILED: threeDaysAheadButtonTouch")
+            return
+        }
+        
+        StarChart.Core.current = StarChart(date: date, coordinates: StarChart.Core.current.coordinates)
+        ResonanceReportViewController.current?.update()
+        ResonanceReportViewController.current?.renderStarChart()
+        
+        if let p = self.parent {
+            p.dismiss(animated: true)
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
+    
 }
